38faa86bf6a83a6f7afbc9dca88dcc821021a859
ijayden-lung <ijayden.lung@gmail.com>
2021-08-04 16:40:43 +0300

    update

---
+++ .bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/.bak/cnda/gencode/merge_db_ens.pl.2021072216.pl.2021072316.pl.2021072416.pl.2021072516.pl.2021072616.pl.2021072716.pl.2021072816.pl.2021072916.pl.2021073016.pl.2021073116.pl.2021080116.pl.2021080216.pl.2021080316.pl.2021080416.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+my $threshold = 49;
+
+my %db_hash;
+open FILE,"/home/longy/workspace/apa_predict/pas_dataset/hg38.pAs.tianbin.txt";
+<FILE>;
+while(<FILE>){
+	chomp;
+	my @data = split;
+	my ($chr,$strand) = @data[2,4];
+	push @{$db_hash{"$chr:$strand"}},\@data;
+}
+
+my %ens_hash;
+my $num;
+open FILE,"zcat gencode.v38.annotation.gtf.gz | awk '(\$3==\"transcript\")' | ";
+while(<FILE>){
+	chomp;
+	my ($chr,$start,$end,$strand,$symbol) = (split)[0,3,4,6,15];
+	$symbol = s/\"|\;//g;
+	$num++;
+	my $pos = $end;
+	if($strand eq "-"){
+		$pos = $start;	
+	}
+	my $pas_id = "$chr:$pos:$strand";
+	my @data = ($pas_id,'Gencode',$chr,$pos,$strand,$symbol);
+	push @{$ens_hash{"$chr:$strand"}},\@data;
+}
+
+open OUT,">THLE2_Control.pAs.merge.coverage.txt";
+while(my ($key,$val) = each %db_hash){
+	my @db_data = @$val;
+	my $val2 = $ens_hash{$key};
+	if(!exists $ens_hash{$key}){
+		print "$key\n";
+		next;
+	}
+
+	my @ens_data = @$val2;
+	my $j = 0;
+	my $db_pre = 0;
+	for(my $i=0;$i<@db_data;$i++){
+		my $db_ref = $db_data[$i];
+		my $ens_ref = $ens_data[$j];
+		my $db_pos = $db_ref->[3];
+		my $ens_pos = $ens_ref->[3];
+		while($j<@ens_data && $ens_pos < $db_pos-$threshold){
+			if($ens_pos > $db_pre+$threshold){
+				print OUT join("\t",@$ens_ref),"\n";
+			}
+			$j++;
+			$ens_ref = $ens_data[$j];
+			$ens_pos = $ens_ref->[3];
+		}
+		print OUT join("\t",@$db_ref),"\n";
+		$db_pre = $db_pos;
+	}
+}
